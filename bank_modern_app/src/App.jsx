fcf1bce12b8c579de366279a822c0ba8611f6505
ArenRitz <aren.dolbakian@gmail.com>
2022-08-12 22:02:14 -0400

    Add Tailwind and initial configuration

---
+++ bank_modern_app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div class="bg-primary w-full overflow-hidden">
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App
+
+
